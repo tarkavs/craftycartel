91878fcf9a3ee9a28c04cdd3cb656c67d5b7dd02
Tarkav Sunday Benjamin <107217907+tarkavs@users.noreply.github.com>
2023-11-03 01:32:18 +0100

    my portfolio

---
+++ e-commerce/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+     <div className="p-6">
+        <Button>click me</Button>
+      </div>
+    )
+  }
